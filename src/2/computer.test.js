5bf59a40fc87243712c47b50fcb8c1aaeaef9b05
Micke Jagekrans <micke.jagekrans@gmail.com>
2019-12-02 22:00:06 +0100

    added more test cases

--- src/2/computer.test.js
+++ src/2/computer.test.js
@@ -9,6 +9,15 @@ describe.only('computer', () => {
   }, {
     input: '1,0,0,0,99',
     output: '2,0,0,0,99',
+  }, {
+    input: '2,3,0,3,99',
+    output: '2,3,0,6,99',
+  }, {
+    input: '2,4,4,5,99,0',
+    output: '2,4,4,5,99,9801',
+  }, {
+    input: '1,1,1,4,99,5,6,0,99',
+    output: '30,1,1,4,2,5,6,0,99',
   }].forEach(({ input, output }) => {
     context(`given ${input}`, () => {
       it(`should return ${output}`, () => {
